c8caa686095816eeee14c6399e623305604f99c1
Md. Nayeem Bhuiyan <bhuiyan9599@gmail.com>
2021-03-09 17:59:10 +0600

    Update

--- web/database/auctionbd.sql
+++ web/database/auctionbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2021 at 01:59 PM
+-- Generation Time: Mar 09, 2021 at 12:58 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.27
 
